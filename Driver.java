6d80309b92387fffb81e8c7114a8544394249ad7
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2020-11-13 13:58:17 -0800

    added comments + reworked constructor w/ degree

--- Driver.java
+++ Driver.java
@@ -1,3 +1,10 @@
+/**
+ * (CS-310 / Program 3 - N-Grams and Frequency Counts) Driver Class
+ * Class that prints internal functioning of FrequencyCount objects, allowing for visual examiantion
+ * @author Anthony Norderhaug
+ * @date 11/13/2020
+ */
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,21 +15,24 @@ import java.util.List;
 public class Driver {
 
     public static void main(String[] args) {
+        // within try-catch block to account for errors
         try {
+            // checking if current arguments follow desired usage
             if (args.length != 1) {
                 throw new IllegalArgumentException();
             }
             List<String> tokens = extractContents(args[0]);
+            // throws IOException, including FileNotFoundException, in case file not found or insufficient user perms
 
-            FrequencyCount freqCount = new FrequencyCount(tokens);
-            FrequencyCount freqCountDegree4 = new FrequencyCount(tokens, 4);
+            FrequencyCount freqCount = new FrequencyCount(tokens); // creating Frequency Count with solely tokens
+            FrequencyCount freqCountDegree4 = new FrequencyCount(tokens, 4); // creating to degree of 4
 
             System.out.println("\nX----------------------FREQUENCY COUNT, DEGREE OF 1----------------------X");
             printHeadAndTail(freqCount);
 
             System.out.println("\nX----------------------FREQUENCY COUNT, DEGREE OF 4----------------------X");
             printHeadAndTail(freqCountDegree4);
-            generatePoem(freqCountDegree4);
+            generatePoem(freqCountDegree4); // call to helper method, generates poem
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found.\n");
@@ -34,9 +44,15 @@ public class Driver {
             System.out.println("File can't be opened.\n");
             e.printStackTrace();
         }
-
     }
 
+    /**
+     * extractContents() derives Scanner from String filename and populates List contents. Scanner is closed.
+     *
+     * @param filename                                      String representing desired file
+     * @return                                              List<String> containing all individual tokens
+     * @throws IOException                                  Exception in case file not found or insufficient perms
+     */
     private static List<String> extractContents(String filename) throws IOException {
         ArrayList<String> contents = new ArrayList<>();
         Scanner input = new Scanner(new FileInputStream(filename));
@@ -49,6 +65,12 @@ public class Driver {
         return contents;
     }
 
+    /**
+     * printHeadAndTail pulls and formats data from FrequencyCount's head() and tail() files. Includes frequency and
+     * percentage compared to all present data
+     *
+     * @param freqCount                                     FrequencyCount object to be used
+     */
     private static void printHeadAndTail(FrequencyCount freqCount) {
         List<String> mostFrequent = freqCount.head();
         List<String> leastFrequent = freqCount.tail();
@@ -78,6 +100,11 @@ public class Driver {
         }
     }
 
+    /**
+     * generatePoem() pulls 4 random tokens and forms a text poem, formatting tokens 1-2-1
+     *
+     * @param freqCount                                     FrequencyCount object to be used
+     */
     private static void generatePoem(FrequencyCount freqCount) {
 
         System.out.println("\nGenerating poem based on 4 random tokens...");
--- FrequencyCount.java
+++ FrequencyCount.java
@@ -1,21 +1,40 @@
+/**
+ * (CS-310 / Program 3 - N-Grams and Frequency Counts) FrequencyCount Class
+ * Class using HashMap to store Strings in a List and track each'S frequency with associated Integers
+ * @author Anthony Norderhaug
+ * @date 11/13/2020
+ */
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class FrequencyCount {
+    // HashMap field, acts as base for FrequencyCount object
     private HashMap<String, Integer> frequencies = new HashMap<>();
 
+    /**
+     * constructor with input List and no degree specification, defaults to 1 and initializes class HashMap
+     *
+     * @param text                                              input List of tokens
+     */
     public FrequencyCount(List<String> text) {
         initializeMap(text);
     }
 
+    /**
+     * constructor with input List and specified degree. Forms List degreeTokens based off on inputList and degree.
+     * Initializes class HashMap with degreeTokens
+     *
+     * @param text                                              input List of tokens
+     * @param degree                                            int representing degree
+     */
     public FrequencyCount(List<String> text, int degree) {
         String current = "";
-        int size = text.size();
+        ArrayList<String> degreeTokens = new ArrayList<String>();
         int startIndex = 0;
 
-        while (degree + startIndex <= size) {
+        while (degree + startIndex <= text.size()) {
             for (int i = 0; i < degree; i++) {
                 if (i == 0) {
                     current += text.get(i + startIndex);
@@ -23,20 +42,29 @@ public class FrequencyCount {
                     current += " " + text.get(i + startIndex);
                 }
             }
-            text.add(current);
+            degreeTokens.add(current);
             current = "";
             startIndex++;
         }
-        text = text.subList(size, size + startIndex);
-        initializeMap(text);
+        initializeMap(degreeTokens);
     }
 
+    /**
+     * initializeMap adds each token within List text to the class HashMap
+     * @param text
+     */
     private void initializeMap(List<String> text) {
         for (String token: text) {
             add(token);
         }
     }
 
+    /**
+     * sortedTokens() creates ArrayList based off of class HashMap's keySet and sorts. Tokens with larger frequencies in front,
+     * lower frequencies closer to end.
+     *
+     * @return                                                 ArrayList representing sorted tokens
+     */
     private ArrayList<String> sortedTokens() {
         ArrayList<String> tokens = new ArrayList<String>(frequencies.keySet());
         String curToken;
@@ -58,6 +86,12 @@ public class FrequencyCount {
         return tokens;
     }
 
+    /**
+     * head() uses SortedTokens() to return subList of first 20 terms, i.e 20 most frequent. If class HashMap's size < 20,
+     * entire list is returned.
+     *
+     * @return                                                 List representing sorted terms (first 20 or entire List)
+     */
     public List<String> head() {
         ArrayList<String> sortedTokens = sortedTokens();
 
@@ -68,6 +102,12 @@ public class FrequencyCount {
         }
     }
 
+    /**
+     * tail() uses SortedTokens() to return subList of last 20 terms, i.e 20 least frequent. If class HashMap's size < 20,
+     * entire list is returned.
+     *
+     * @return                                                 List representing sorted terms (last 20 or entire List)
+     */
     public List<String> tail() {
         ArrayList<String> sortedTokens = sortedTokens();
 
@@ -78,6 +118,12 @@ public class FrequencyCount {
         }
     }
 
+    /**
+     * randomToken() returns a random token in frequencies. Facilitated by randGen's randomized index used within ArrayList
+     * of unique tokens.
+     *
+     * @return                                                  Random token retrieved by randGen
+     */
     public String randomToken() {
         ArrayList<String> tokens = new ArrayList<String>(frequencies.keySet());
         Random randGen = new Random();
@@ -85,10 +131,23 @@ public class FrequencyCount {
         return tokens.get(randGen.nextInt(tokens.size()));
     }
 
+    /**
+     * count() returns frequency of the input token. If not present, 0 is returned.
+     *
+     * @param token                                             String representing desired token
+     * @return                                                  int representing found frequency
+     */
     public int count(String token) {
         return frequencies.getOrDefault(token, 0);
     }
 
+    /**
+     * percent() takes entire frequency sum of base HashMap and divides input token's frequency by it. Returns value,
+     * representing desired token's held percentage compared to all present words
+     *
+     * @param token                                             String representing desired token
+     * @return                                                  int representing percentage amongst all present words
+     */
     public double percent(String token) {
         int frequencySum = 0;
 
@@ -99,6 +158,13 @@ public class FrequencyCount {
         return (frequencies.get(token) * 1.0)/frequencySum;
     }
 
+    /**
+     * add() adds an input token into the base HashMap. If a token is already present, it updates frequency by +1. If
+     * not, it adds onto with starting frequency of 1.
+     *
+     * @param token                                             String representing desired token
+     * @return                                                  Frequency after token addition
+     */
     public int add(String token) {
         if (frequencies.containsKey(token)) {
             frequencies.replace(token, frequencies.get(token) +1);
